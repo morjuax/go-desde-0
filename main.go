1c43d95dc5f91c600a909a12e91e504ef7a18b75
Juan Moreno <jcarlmorenos@falabella.cl>
2025-01-10 18:05:07 -0300

    chore: rest variables

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/morjuax/go-desde-0/variables"
 
 func main() {
-	variables.ShowIntegers()
+	variables.RestVariables()
 }
---
+++ variables/rest.go
@@ -0,0 +1,22 @@
+package variables
+
+import (
+	"fmt"
+	"time"
+)
+
+var Name string
+var State bool
+var Salary float32
+var Date time.Time
+func RestVariables() {
+	Name = "Juan"
+	State = true;
+	Salary  = 1577.66
+	Date = time.Now()
+
+	fmt.Println(Name)
+	fmt.Println(State)
+	fmt.Println(Salary)
+	fmt.Println(Date)
+}
